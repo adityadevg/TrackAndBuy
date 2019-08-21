a70cb074fdeb69d8be21ce62c6c35af91de89d9f
adityadev91 <adityadev.india@gmail.com>
2019-08-20 21:19:53 -0400

    Updated setup file to install node npm and nvm if not present

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Install npm dependencies
+
+# Install all required dependencies
+# Nodejs
+sudo apt install nodejs npm
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# This loads nvm and bash_completion in variables
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Reload bash environment
+source ~/.bashrc
+source ~/.bash_profile
+
+# npm
 npm i --prefix ./server graphql apollo-server apollo-datasource-rest --save
+
 
 # Request API Key from user
 echo "Enter your AlphaVantage Key. You can find your key by going to
@@ -10,6 +28,7 @@ Default Value (demo)"
 read apikey
 apikey=${apikey:-demo}
 echo "module.exports = { apikey: '"$apikey"'};" > server/vars.env
+
 
 # Execute the application
 node server/server.js
