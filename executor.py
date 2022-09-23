0c6897574789e5b804f221d24d32f617b66315dc
Aditya Dev <6170855+adityadevg@users.noreply.github.com>
2022-09-22 20:41:00 -0400

    Use `scheduled_job` decorator instead of `add_job`

--- executor.py
+++ executor.py
@@ -5,7 +5,7 @@ from main import Main
 
 scheduler = BlockingScheduler()
 
-@scheduler.add_job("cron",day_of_week="mon-fri",hour="17")
+@scheduler.scheduled_job("cron",day_of_week="mon-fri",hour="17")
 def scheduled_job():
     for symbol in SYMBOLS:
         Main(symbol)
